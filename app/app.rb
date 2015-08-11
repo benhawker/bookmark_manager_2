adb4afeaafc416121f0b105252f2ea5294fa9f1d
Ben <ben.c.hawker@gmail.com>
2015-08-11 11:33:28 +0100

    Add

--- app/app.rb
+++ app/app.rb
@@ -7,8 +7,17 @@ class App < Sinatra::Base
 
   get '/links' do
   	@links = Link.all
-  	erb :'links/index'
+  	erb :'/links/index'
   end 
+
+  get '/links/new' do
+  	erb :'links/new'
+  end
+
+  post '/links' do
+  	Link.create(url: params[:url], title: params[title])
+  	redirect to('/links')
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
